99aa3c760e77da9d44331ea1fdc681a6d91094b1
CN88H <cheenou88@gmail.com>
2022-08-01 04:07:29 -0500

    Created Database and adjust settings + Created music libary app.

---
+++ music_library_project/local_setting.py
@@ -0,0 +1,15 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-n&flp5sknml#*ji(%zvm5)#n=n*f4f264^5p4)+1e%s2g7y^98'
+
+# Database
+# https://docs.djangoproject.com/en/4.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'music_library_database',
+        'HOST': 'localhost',
+        'USER': 'root',
+        'PASSWORD': 'Damnbaka.88'
+    }
+}
