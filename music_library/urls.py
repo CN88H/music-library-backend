16e59e2424342f749a135ac0c62caa3ae97c56a7
CN88H <cheenou88@gmail.com>
2022-08-01 18:21:14 -0500

    Created Get by Id function.

--- music_library/urls.py
+++ music_library/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('music/', views.music_list),
+    path('', views.music_list),
+    path('<int:pk>', views.music_detail),
 ]
--- music_library/views.py
+++ music_library/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -12,9 +13,14 @@ def music_list(request):
         serializer = MusicLibrarySerializer(musics, many=True)
         return Response(serializer.data)
 
-
     elif request.method == 'POST':
         serializer = MusicLibrarySerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def music_detail(request, pk):
+        music = get_object_or_404(MusicLibary, pk=pk)
+        serializer = MusicLibrarySerializer(music)
+        return Response(serializer.data)
