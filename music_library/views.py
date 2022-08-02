8dbccd1e9068f000d3a6efb69db055529f078afd
CN88H <cheenou88@gmail.com>
2022-08-02 03:12:49 -0500

    Created Update function in the app views file.

--- music_library/views.py
+++ music_library/views.py
@@ -19,8 +19,16 @@ def music_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'Put'])
 def music_detail(request, pk):
-        music = get_object_or_404(MusicLibary, pk=pk)
-        serializer = MusicLibrarySerializer(music)
-        return Response(serializer.data)
+        if request.method == 'GET':
+            music = get_object_or_404(MusicLibary, pk=pk)
+            serializer = MusicLibrarySerializer(music)
+            return Response(serializer.data)
+        elif request.method == 'PUT':
+            music = get_object_or_404(MusicLibary, pk=pk)
+            serializer = MusicLibrarySerializer(music, data=request.data)
+            serializer.is_valid(raise_exception=True)
+            serializer.save()
+            return Response(serializer.data)
+
